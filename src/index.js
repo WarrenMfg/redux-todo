56cd1e7377f43823e26d729da1c2f940d433355d
WarrenMfg <warrenmfg@icloud.com>
2020-05-19 09:59:23 -0400

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import store from './store';
 import App from './App';
 
+const persistor = persistStore(store);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+  <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+    <App />
+  </PersistGate>
+</Provider>
+, document.getElementById('root'));
