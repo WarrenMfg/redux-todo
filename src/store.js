201964b01725ed906d82822a6db7457dd887faa8
WarrenMfg <warrenmfg@icloud.com>
2020-05-19 11:24:07 -0400

    added thunk packages and implemented in store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import thunk from 'redux-thunk';
 import { todos } from './todos/reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducers = {
   todos
@@ -19,5 +21,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default createStore(
   persistedReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
 );
